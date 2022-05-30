7ee85cea1b6b9dd9a2b6ec68a878f6f675f3c0c9
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-05-30 15:57:35 -0300

    Main Update Login

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 from Funcionario.cadastro import Funcionario
+import os
+from time import sleep
+from Funcionario.login import login
+
 
 
 while True:
-    menu= Funcionario.menu("Login","Criar Conta", 'Sair')
+    menu= Funcionario.menu("Menu Principal", "Login","Criar Conta", 'Sair')
     if menu == 1: #Logar conta
-        pass
+        login = str(input('Nome: '))
+        login_senha = str(input('Senha: '))
+
+        if login == Funcionario.funcionarios['Nome']:
+            if Funcionario.funcionarios['Senha'] == login_senha:
+                login(True)
+            else:
+                print('Senha Inválida.')
+                sleep(1)
+                os.system('cls' if os.name == 'nt' else 'clear')
+                continue
+    
+    
     elif menu == 2: #Criar conta a ser definido
         Funcionario.cadastra_funcionario(Funcionario)
+        sleep(1)
+        os.system('cls' if os.name == 'nt' else 'clear')  #clear terminal
     elif menu == 3:
         print('\033[31mFechando Programa.\033[m')
+        sleep(1)
+        os.system('cls' if os.name == 'nt' else 'clear')
         break
-    elif menu == 4:
-        Funcionario.show_funcionarios()
+
